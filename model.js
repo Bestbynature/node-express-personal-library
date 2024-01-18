7cd982db9d50ee35f01f83ae69345a58120790d2
Dammy <bestbynature2010@gmail.com>
2024-01-18 16:58:42 +0100

    Add the model and schema for the books

---
+++ model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {type: String, required: true},
+  comments: [String],
+  commentcount: {type: Number, default: 0}
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+exports.Book = Book;
